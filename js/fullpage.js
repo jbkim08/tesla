c839e06fa09d6a429b08b95addcec070f4d27713
jbkim08 <jbkim08@gmail.com>
2022-03-17 15:25:19 +0900

    Update fullpage.js

--- js/fullpage.js
+++ js/fullpage.js
@@ -3458,8 +3458,8 @@
             var msgStyle = 'font-size: 15px;background:yellow;';
 
             if(!isOK){
-                showError('error', 'Fullpage.js version 3 has changed its license to GPLv3 and it requires a `licenseKey` option. Read about it here:');
-                showError('error', 'https://github.com/alvarotrigo/fullPage.js#options');
+                //showError('error', 'Fullpage.js version 3 has changed its license to GPLv3 and it requires a `licenseKey` option. Read about it here:');
+                //showError('error', 'https://github.com/alvarotrigo/fullPage.js#options');
             }
             else if(l && l.length < 20){
                 console.warn('%c This website was made using fullPage.js slider. More info on the following website:', msgStyle);
